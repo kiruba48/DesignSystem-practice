e651a58a12bffd6bd7b2e38da8c5568ffaf3f5fb
Kiruba Muthupalani <kiruba@Kirubas-MBP.home>
2024-04-30 15:52:24 +0100

    storybook button initial setup

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Button } from './button';
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  args: {
+    children: 'Button',
+  },
+} satisfies Meta;
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+  },
+};
